8d62a492cfa437845dddff9be3c3927346b86dc0
wangyucheng <wangyucheng@kylinsec.com.cn>
2023-03-23 20:46:41 +0800

    fix(translate):add some translation
    - 添加一些翻译
    
    Relates #67661

---
+++ translations/pam_kiran_authentication.mn_MN.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/pam/authentication-terminal.cpp" line="34"/>
+        <source>Select Authentication type (%1 default, %2 password, %3 fingerprint): </source>
+        <translation>Гэрчлэлийн төрөл% 1 анхдагч% 2 нууц үг% 3 хурууны хээг сонгоно уу</translation>
+    </message>
+    <message>
+        <location filename="../src/pam/authentication.cpp" line="102"/>
+        <source>Too many authentication failures, so the authentication mode is locked.</source>
+        <translation>Хэт олон баталгаажуулалт амжилтгүй болсон тул гэрчлэх горим түгжигдсэн байна</translation>
+    </message>
+</context>
+</TS>
