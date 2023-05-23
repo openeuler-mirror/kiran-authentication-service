4ebbdfebe5c85c58f7f69606346a374ee6f2bba8
liuxinhao <liuxinhao@kylinsec.com.cn>
2023-05-23 11:39:28 +0800

    refactor(ts&pam service): Update translation file and modify the pam configuration file
    - 更新翻译文件，修改PAM配置文件

--- translations/pam_kiran_authentication.zh_CN.ts
+++ translations/pam_kiran_authentication.zh_CN.ts
@@ -4,7 +4,7 @@
 <context>
     <name>Kiran::Authentication</name>
     <message>
-        <location filename="../src/pam/authentication.cpp" line="120"/>
+        <location filename="../src/pam/authentication.cpp" line="123"/>
         <source>Too many authentication failures, so the authentication mode is locked.</source>
         <translation>错误次数过多，认证模式已被禁用。</translation>
     </message>
@@ -12,60 +12,68 @@
 <context>
     <name>Kiran::AuthenticationTerminal</name>
     <message>
-        <location filename="../src/pam/authentication-terminal.cpp" line="51"/>
-        <source>passwd</source>
-        <translation>密码认证</translation>
-    </message>
-    <message>
-        <location filename="../src/pam/authentication-terminal.cpp" line="52"/>
-        <source>fingerprint</source>
-        <translation>指纹认证</translation>
-    </message>
-    <message>
-        <location filename="../src/pam/authentication-terminal.cpp" line="53"/>
-        <source>face</source>
-        <translation>人脸认证</translation>
-    </message>
-    <message>
-        <location filename="../src/pam/authentication-terminal.cpp" line="54"/>
-        <source>ukey</source>
-        <translation>UKey认证</translation>
-    </message>
-    <message>
-        <location filename="../src/pam/authentication-terminal.cpp" line="55"/>
-        <source>fingervein</source>
-        <translation>指静脉认证</translation>
-    </message>
-    <message>
-        <location filename="../src/pam/authentication-terminal.cpp" line="77"/>
+        <location filename="../src/pam/authentication-terminal.cpp" line="69"/>
         <source>Select Authentication type (%1): </source>
         <translation>请选择认证类型（%1）： </translation>
     </message>
     <message>
-        <location filename="../src/pam/authentication-terminal.cpp" line="94"/>
+        <location filename="../src/pam/authentication-terminal.cpp" line="86"/>
         <source>The authentication type is invalid. Please select a new one</source>
         <translation>该认证类型无效，请重新选择</translation>
     </message>
 </context>
 <context>
+    <name>QCoreApplication</name>
+    <message>
+        <location filename="../src/utils/utils.cpp" line="194"/>
+        <source>password</source>
+        <translation>密码</translation>
+    </message>
+    <message>
+        <location filename="../src/utils/utils.cpp" line="195"/>
+        <source>fingerprint</source>
+        <translation>指纹</translation>
+    </message>
+    <message>
+        <location filename="../src/utils/utils.cpp" line="196"/>
+        <source>face</source>
+        <translation>人脸</translation>
+    </message>
+    <message>
+        <location filename="../src/utils/utils.cpp" line="197"/>
+        <source>fingervein</source>
+        <translation>指静脉</translation>
+    </message>
+    <message>
+        <location filename="../src/utils/utils.cpp" line="198"/>
+        <source>iris</source>
+        <translation>虹膜</translation>
+    </message>
+    <message>
+        <location filename="../src/utils/utils.cpp" line="199"/>
+        <source>ukey</source>
+        <translation>UKey</translation>
+    </message>
+</context>
+<context>
     <name>QObject</name>
     <message>
-        <location filename="../src/utils/utils.cpp" line="183"/>
+        <location filename="../src/utils/utils.cpp" line="217"/>
         <source>Feature not match.</source>
         <translation>特征不匹配。</translation>
     </message>
     <message>
-        <location filename="../src/utils/utils.cpp" line="185"/>
+        <location filename="../src/utils/utils.cpp" line="219"/>
         <source>Feature matching successed.</source>
         <translation>特征匹配成功。</translation>
     </message>
     <message>
-        <location filename="../src/utils/utils.cpp" line="187"/>
+        <location filename="../src/utils/utils.cpp" line="221"/>
         <source>Feature not match, please retry it.</source>
         <translation>特征不匹配，请重试。</translation>
     </message>
     <message>
-        <location filename="../src/utils/utils.cpp" line="190"/>
+        <location filename="../src/utils/utils.cpp" line="226"/>
         <source>Unknown verfication error.</source>
         <translation>未知认证错误。</translation>
     </message>
