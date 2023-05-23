3aa69f2e7698b89cc727aeee16b098c9ac53ded9
liuxinhao <liuxinhao@kylinsec.com.cn>
2023-05-23 11:39:28 +0800

    refactor(ts&pam service): Update translation file and modify the pam configuration file
    - 更新翻译文件，修改PAM配置文件

--- translations/kiran-authentication-daemon.zh_CN.ts
+++ translations/kiran-authentication-daemon.zh_CN.ts
@@ -10,16 +10,72 @@
     </message>
 </context>
 <context>
+    <name>Kiran::Session</name>
+    <message>
+        <location filename="../src/daemon/session.cpp" line="201"/>
+        <source>Please wait while the %1 request is processed</source>
+        <translation>%1认证请求正在等待处理</translation>
+    </message>
+    <message>
+        <location filename="../src/daemon/session.cpp" line="281"/>
+        <source>Insert the UKey and enter the PIN code</source>
+        <translation>请插入UKey并输入PIN码</translation>
+    </message>
+    <message>
+        <location filename="../src/daemon/session.cpp" line="282"/>
+        <source>please input ukey code.</source>
+        <translation>请输入PIN码。</translation>
+    </message>
+</context>
+<context>
     <name>Kiran::User</name>
     <message>
-        <location filename="../src/daemon/user.cpp" line="165"/>
+        <location filename="../src/daemon/user.cpp" line="162"/>
+        <source>Please wait while the request is processed</source>
+        <translation>请等待,录入请求正在处理</translation>
+    </message>
+    <message>
+        <location filename="../src/daemon/user.cpp" line="168"/>
         <source>Enroll has been interrupted. Please wait</source>
         <translation>录入中断请稍等</translation>
     </message>
     <message>
-        <location filename="../src/daemon/user.cpp" line="171"/>
+        <location filename="../src/daemon/user.cpp" line="174"/>
         <source>Enroll has been cancelled</source>
         <translation>录入已被取消</translation>
+    </message>
+</context>
+<context>
+    <name>QCoreApplication</name>
+    <message>
+        <location filename="../src/utils/utils.cpp" line="194"/>
+        <source>password</source>
+        <translation>密码</translation>
+    </message>
+    <message>
+        <location filename="../src/utils/utils.cpp" line="195"/>
+        <source>fingerprint</source>
+        <translation>指纹</translation>
+    </message>
+    <message>
+        <location filename="../src/utils/utils.cpp" line="196"/>
+        <source>face</source>
+        <translation>人脸</translation>
+    </message>
+    <message>
+        <location filename="../src/utils/utils.cpp" line="197"/>
+        <source>fingervein</source>
+        <translation>指静脉</translation>
+    </message>
+    <message>
+        <location filename="../src/utils/utils.cpp" line="198"/>
+        <source>iris</source>
+        <translation>虹膜</translation>
+    </message>
+    <message>
+        <location filename="../src/utils/utils.cpp" line="199"/>
+        <source>ukey</source>
+        <translation>UKey</translation>
     </message>
 </context>
 <context>
@@ -36,51 +92,56 @@
     </message>
     <message>
         <location filename="../src/daemon/error.cpp" line="36"/>
+        <source>User Feature limits exceeded</source>
+        <translation>用户特征已到上限</translation>
+    </message>
+    <message>
+        <location filename="../src/daemon/error.cpp" line="39"/>
         <source>Too many sessions.</source>
         <translation>过多认证会话。</translation>
     </message>
     <message>
-        <location filename="../src/daemon/error.cpp" line="39"/>
+        <location filename="../src/daemon/error.cpp" line="42"/>
         <source>The session is in authentication.</source>
         <translation>该会话正在认证中。</translation>
     </message>
     <message>
-        <location filename="../src/daemon/error.cpp" line="42"/>
+        <location filename="../src/daemon/error.cpp" line="45"/>
         <source>Internel error.</source>
         <translation>内部错误。</translation>
     </message>
     <message>
-        <location filename="../src/daemon/error.cpp" line="45"/>
+        <location filename="../src/daemon/error.cpp" line="48"/>
         <source>No Such Device.</source>
         <translation>不存在该类型设备。</translation>
     </message>
     <message>
-        <location filename="../src/daemon/error.cpp" line="48"/>
+        <location filename="../src/daemon/error.cpp" line="51"/>
         <source>Unknown error.</source>
         <translation>未知错误。</translation>
     </message>
     <message>
-        <location filename="../src/daemon/error.cpp" line="52"/>
+        <location filename="../src/daemon/error.cpp" line="55"/>
         <source> (error code: 0x%x)</source>
         <translation> (错误码:0x%x)</translation>
     </message>
     <message>
-        <location filename="../src/utils/utils.cpp" line="183"/>
+        <location filename="../src/utils/utils.cpp" line="217"/>
         <source>Feature not match.</source>
         <translation>特征不匹配。</translation>
     </message>
     <message>
-        <location filename="../src/utils/utils.cpp" line="185"/>
+        <location filename="../src/utils/utils.cpp" line="219"/>
         <source>Feature matching successed.</source>
         <translation>特征匹配成功。</translation>
     </message>
     <message>
-        <location filename="../src/utils/utils.cpp" line="187"/>
+        <location filename="../src/utils/utils.cpp" line="221"/>
         <source>Feature not match, please retry it.</source>
         <translation>特征不匹配，请重试。</translation>
     </message>
     <message>
-        <location filename="../src/utils/utils.cpp" line="190"/>
+        <location filename="../src/utils/utils.cpp" line="226"/>
         <source>Unknown verfication error.</source>
         <translation>未知认证错误。</translation>
     </message>
